fd6a6ecb40d067b4fab80366e04e5533f112d145
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-06-26 18:00:30 +0000

    fix code error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Example showing how to resolve with a dummy response after a short delay
-        setTimeout(() => {
-            resolve({ data: 'Dummy response from API' });
-        }, 1000);
-    });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-module.exports = {
-    getResponseFromAPI
-};
